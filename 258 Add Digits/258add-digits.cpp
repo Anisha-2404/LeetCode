da1ea51a5a5fa971082baeacc4e1bfd4681a5d5d
Anisha-2404 <anishajain2404@gmail.com>
2026-01-31 05:18:03 +0000

    Code challenge submission for question: 258 Add Digits

---
+++ 258 Add Digits/258add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while (num >= 10) {        // more than one digit
+            int sum = 0;
+            
+            while (num > 0) {
+                sum += num % 10;   // add last digit
+                num /= 10;        // remove last digit
+            }
+            
+            num = sum;            // repeat with new sum
+        }
+        return num;
+    }
+};
