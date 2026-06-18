bc03c3c69c093e325b816e5d0c4acb5a12ca8e82
Anisha-2404 <anishajain2404@gmail.com>
2026-06-18 06:14:57 +0000

    Code challenge submission for question: 283 Move Zeroes

---
+++ 283 Move Zeroes/283move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] k = new int[n];
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                k[index++] = nums[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = k[i];
+        }
+    }
+}
